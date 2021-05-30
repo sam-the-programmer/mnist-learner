6d1ca0c84650bef0165efd5db477b1f59e5415a9
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-05-30 09:56:46 +0100

    Changed Data Types

--- process_data.py
+++ process_data.py
@@ -27,16 +27,19 @@ for image in raw:                     # Iterate through each image
     image.pop(0)                      # Remove from list of the image
 
 for image in raw:
-    train_samples.append(image)
+    train_samples.append(image)       # Add each image's data to the samples
 
 for image in range(len(train_samples)):
     for pixel in range(len(train_samples[image])):
-        train_samples[image][pixel] = int(train_samples[image][pixel])
+        train_samples[image][pixel] = int(train_samples[image][pixel]) # Turn all samples into integers
 
+# Turn all data and labels into integers
 for label in range(len(train_labels)):
-    for i in range(len(train_labels[label])):
-        train_labels[label][i] = int(train_labels[label][i])
-
+    if type(train_labels[label]) == list:
+        for i in range(len(train_labels[label])):
+            train_labels[label][i] = int(train_labels[label][i])
+    elif type(train_labels[label]) == int:
+        train_labels[label] = int(train_labels[label])
 
 print('Shaping data...', end='\r')
 
